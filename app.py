6154916f71c4f6ed8eb6b530f2004499ba837010
dfcano <85811161+dfcano@users.noreply.github.com>
2023-05-22 23:22:05 -0500

    Update app.py
    Update Api Key

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 # creating an API object
 api = Api(app)
 # create database
-app.config['SECRET_KEY']='rnd_J0KdSHCYfewCs3vbocTfISnSNCpe'
+app.config['SECRET_KEY']='rnd_bjltc3TJgA9zKBPC7rY7HJsSw1MY'
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # sqlalchemy mapper
